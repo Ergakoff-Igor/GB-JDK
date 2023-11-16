02d520b34caa1d5a07a77b5db9069da3bddf73ab
Igor <i.ergakoff@mail.ru>
2023-11-16 22:44:50 +0300

    GB. JDK. Lecture 1

---
+++ src/main/java/ru/gb/ergakov/lesson1/lecture1/Main.java
@@ -0,0 +1,9 @@
+package ru.gb.ergakov.lesson1.lecture1;
+
+import ru.gb.ergakov.lesson1.lecture1.GameWindow;
+
+public class Main {
+    public static void main(String[] args) {
+        new GameWindow();
+    }
+}
